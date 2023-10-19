156fbfce87ddbc0d20f6df1b5b13dbe229851f43
jamiul <jamiulewu24@gmail.com>
2023-10-19 11:16:54 +0600

    Todo read and search

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -5,11 +5,15 @@ namespace App\Livewire;
 use App\Models\Todo;
 use Livewire\Attributes\Rule;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class TodoList extends Component
 {
+    use WithPagination;
+
     #[Rule('required|min:3|max:50')]
     public $name;
+    public $search;
 
     public function create()
     {
@@ -24,6 +28,7 @@ class TodoList extends Component
 
     public function render()
     {
-        return view('livewire.todo-list');
+        return view('livewire.todo-list',
+    ['todos' => Todo::latest()->where('name', 'like', "%{$this->search}%")->paginate(5)]);
     }
 }
